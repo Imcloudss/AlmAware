593954b0b03f079de04ab198557dffcba524897c
Andrea <andrea.nuvoli@studio.unibo.it>
2025-07-04 12:13:33 +0200

    Struttura schermata home suddivisa per card (da sfalsare verticalmente)

--- app/src/main/java/com/example/almaware/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/almaware/ui/screens/home/HomeScreen.kt
@@ -1,31 +1,129 @@
 package com.example.almaware.ui.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.almaware.ui.composables.AppBar
 import com.example.almaware.ui.composables.BottomNavigationBar
+import com.example.almaware.ui.screens.home.data.backgrounds
+import com.example.almaware.ui.screens.home.data.borderColors
+import com.example.almaware.ui.screens.home.data.cardOverlays
+
+// Data class per le card
+data class HomeCard(
+    val id: Int,
+    val backgroundRes: Int,
+    val overlayRes: Int,
+    val borderColor: Color,
+    val route: String,
+)
 
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+    val items = (0 until 17).map { index ->
+        HomeCard(
+            id = index + 1,
+            backgroundRes = backgrounds[index],
+            overlayRes = cardOverlays[index],
+            borderColor = borderColors[index],
+            route = "destination${index + 1}"
+        )
+    }
+
     Scaffold(
         topBar = {
             AppBar(
-                "Prova",
+                "Prove",
                 navController
             )
         },
         bottomBar = {
             BottomNavigationBar(navController)
         }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier.padding(paddingValues)
-        ) {  }
+    ) { contentPadding ->
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 80.dp),
+            modifier =  Modifier.padding(contentPadding)
+        ) {
+            items(items) { item ->
+                CardItem(
+                    item,
+                    onClick = { }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CardItem(
+    item: HomeCard,
+    onClick: () -> Unit
+) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .height(270.dp)
+            .width(100.dp)
+            .fillMaxWidth()
+            .border(
+                width = 3.dp,
+                color = item.borderColor,
+                shape = RoundedCornerShape(10.dp)
+            ),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Image(
+                painter = painterResource(id = item.backgroundRes),
+                contentDescription = "Sfondo card${item.id}",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .matchParentSize()
+                    .graphicsLayer { alpha = 0.5f }
+            )
+
+            Image(
+                painter = painterResource(id= item.overlayRes),
+                contentDescription = "Card${item.id}",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(100.dp)
+                    .align(Alignment.Center)
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/almaware/ui/screens/home/data/Lists.kt
@@ -0,0 +1,64 @@
+package com.example.almaware.ui.screens.home.data
+
+import androidx.compose.ui.graphics.Color
+import com.example.almaware.R
+
+val backgrounds = listOf(
+    R.drawable.sfondo1,
+    R.drawable.sfondo2,
+    R.drawable.sfondo3,
+    R.drawable.sfondo4,
+    R.drawable.sfondo5,
+    R.drawable.sfondo6,
+    R.drawable.sfondo7,
+    R.drawable.sfondo8,
+    R.drawable.sfondo9,
+    R.drawable.sfondo10,
+    R.drawable.sfondo11,
+    R.drawable.sfondo6,
+    R.drawable.sfondo13,
+    R.drawable.sfondo6,
+    R.drawable.sfondo15,
+    R.drawable.sfondo16,
+    R.drawable.sfondo17,
+)
+
+val cardOverlays = listOf(
+    R.drawable.card1,
+    R.drawable.card2,
+    R.drawable.card3,
+    R.drawable.card4,
+    R.drawable.card5,
+    R.drawable.card6,
+    R.drawable.card7,
+    R.drawable.card8,
+    R.drawable.card9,
+    R.drawable.card10,
+    R.drawable.card11,
+    R.drawable.card12,
+    R.drawable.card13,
+    R.drawable.card14,
+    R.drawable.card15,
+    R.drawable.card16,
+    R.drawable.card17,
+)
+
+val borderColors = listOf(
+    Color(0xFFE5243B),
+    Color(0xFFDDA63A),
+    Color(0xFF4C9F38),
+    Color(0xFFC5192D),
+    Color(0xFFFF3A21),
+    Color(0xFF26BDE2),
+    Color(0xFFFCC30B),
+    Color(0xFFA21942),
+    Color(0xFFFD6925),
+    Color(0xFFDD1367),
+    Color(0xFFFD9D24),
+    Color(0xFFBF8B2E),
+    Color(0xFF3F7E44),
+    Color(0xFF0A97D9),
+    Color(0xFF56C02B),
+    Color(0xFF00689D),
+    Color(0xFF19486A),
+)
